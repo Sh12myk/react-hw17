52d3aeeccdeaa6c9d088e9a295bfa7926091fc69
Shostak Mykola <mykolashostak@gmail.com>
2021-05-12 22:49:25 +0300

    add header

--- src/App.js
+++ src/App.js
@@ -2,12 +2,9 @@ import './components/style.css';
 import Contacts from './components/Contacts.js';
 function App() {
   return (
-      <header>
         <div>
           <Contacts />
       </div>
-      </header>
-      
   );
 }
 
